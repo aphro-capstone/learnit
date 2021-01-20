924ec96a0761700e4e77475e3df247725b4bc0ea
Aphrodite Gajo <aprogajo@gmail.com>
2021-01-20 21:13:22 +0800

    Update as of 9:13pm

--- application/views/shared/profile.php
+++ application/views/shared/profile.php
@@ -1,41 +1,41 @@
 <div class="container" id="user-profile-page">
 
-	<div class="left">
-		<div class="column-panel-multi-row d-flex">
-			<div class="m-auto">
+		<div class="left">
+			<div class="column-panel-multi-row d-flex">
+				<div class="m-auto">
+						
+
+				 	<div class="profile-avatar-main">
+				 		<div class="img-container"> 
+				 			<img src="<?=base_url() . getSessionData('sess_userImage');?>" alt="" />
+				 		</div>	
+				 		<div class="avatar-profile-overlay">
+				 			<div class="avatar-text-container">
+				 				<svg class="d-block m-auto"width="24" height="24" viewBox="0 0 24 24" name="camera-icon"><path fill="#fff" fill-rule="evenodd" stroke="none" stroke-width="1" id="Icon/small/camera" d="M7.23 5.579C7.569 4.659 8.478 4 9.546 4h4.91c1.068 0 1.977.66 2.314 1.579h2.595c.903 0 1.636.707 1.636 1.579V17.42c0 .872-.733 1.579-1.636 1.579H4.636C3.733 19 3 18.293 3 17.421V7.158c0-.872.733-1.58 1.636-1.58h2.595zM12 16.632c2.711 0 4.91-2.121 4.91-4.737S14.71 7.158 12 7.158c-2.711 0-4.91 2.12-4.91 4.737 0 2.616 2.199 4.737 4.91 4.737zm-3.2-5.4c.236.837 1.03 1.452 1.973 1.452 1.13 0 2.045-.883 2.045-1.973 0-.91-.637-1.676-1.505-1.905a3.4 3.4 0 01.687-.07c1.807 0 3.273 1.415 3.273 3.159 0 1.744-1.466 3.158-3.273 3.158-1.807 0-3.273-1.414-3.273-3.158 0-.228.025-.45.072-.663z"></path></svg>
+								<span class="d-block text-center">Update Photo</span>
+				 			</div>	
+				 		</div>
+				 	</div>
+				 
+					<div class="profile-info-section">
+							<div class="profile-banner-title-container qa-test-profilePageHeader-titleContainer d-flex align-items-baseline">
+								<h4 class="display-full-name pt-1"> <?=getUserName(); ?></h4>
+								<p class="student-profile subtext"></p></div>
+									<span><small class="stu">Student</small></span>
+
+								<ul class="personal-info no-style mt-3 pl-3" >
+								<li class="" > <i class="fa fa-home"></i> Davao del norte State College</li>
+								<span class="icon-edit qa-test-profilePage-editIcon"></span></ul>
+								<button class="btn btn-primary">Edit Profile</button>
+					</div>
 					
 
-			 <div class="profile-avatar-main">
-			 		<div class="img-container"> 
-			 			<img src="<?=base_url() . getSessionData('sess_userImage');?>" alt="" />
-			 		</div>	
-			 		<div class="avatar-profile-overlay">
-			 			<div class="avatar-text-container">
-			 				<svg class="d-block m-auto"width="24" height="24" viewBox="0 0 24 24" name="camera-icon"><path fill="#fff" fill-rule="evenodd" stroke="none" stroke-width="1" id="Icon/small/camera" d="M7.23 5.579C7.569 4.659 8.478 4 9.546 4h4.91c1.068 0 1.977.66 2.314 1.579h2.595c.903 0 1.636.707 1.636 1.579V17.42c0 .872-.733 1.579-1.636 1.579H4.636C3.733 19 3 18.293 3 17.421V7.158c0-.872.733-1.58 1.636-1.58h2.595zM12 16.632c2.711 0 4.91-2.121 4.91-4.737S14.71 7.158 12 7.158c-2.711 0-4.91 2.12-4.91 4.737 0 2.616 2.199 4.737 4.91 4.737zm-3.2-5.4c.236.837 1.03 1.452 1.973 1.452 1.13 0 2.045-.883 2.045-1.973 0-.91-.637-1.676-1.505-1.905a3.4 3.4 0 01.687-.07c1.807 0 3.273 1.415 3.273 3.159 0 1.744-1.466 3.158-3.273 3.158-1.807 0-3.273-1.414-3.273-3.158 0-.228.025-.45.072-.663z"></path></svg>
-							<span class="d-block text-center">Update Photo</span>
-			 			</div>	
-			 		</div>
-			 	</div>
-			 
-			<div class="profile-info-section">
-					<div class="profile-banner-title-container qa-test-profilePageHeader-titleContainer d-flex align-items-baseline">
-						<h4 class="display-full-name pt-1"> <?=getUserName(); ?></h4>
-						<p class="student-profile subtext"></p></div>
-							<span><small class="stu">Student</small></span>
-
-						<ul class="personal-info no-style mt-3 pl-3" >
-						<li class="" > <i class="fa fa-home"></i> Davao del norte State College</li>
-						<span class="icon-edit qa-test-profilePage-editIcon"></span></ul>
-						<button class="btn btn-primary">Edit Profile</button>
+				</div>
 			</div>
-			
-
-			</div>
-		</div>
 
 
-	<div class="container" class="aboutpart">
-		<div class="navigation-list-container navigation-list-container-gray">
+			<div class="container" class="aboutpart">
+				<div class="navigation-list-container navigation-list-container-gray">
 
 					<ul class="navigation-list qa-test-list-group navigation-list-container-horizontal-on-mobile list-group">
 						<li class="p-0 list-group-item">
@@ -57,7 +57,18 @@
 				</div>
 			</div>
 		</div>
-	</div>
+
+
+<div class="right">
+		
+			<div class="panel mt-1">
+				<div class="panel-content">
+						<span>POST</span>
+						
+					</div>
+
+				</div>
+			</div>
 
 <!-- 
 <div class="profile-page-header p-3 pt-6 pb-6">
@@ -102,20 +113,6 @@
 
 
 
-
-<div class="col col-sm-12  col-md-6 col-lg-6 pr-6" style="
-    margin: 0px;
-    padding: 16px;
-">
-		
-			<div class="panel mt-1">
-				<div class="panel-content">
-						<span>POST</span>
-						
-					</div>
-
-				</div>
-			</div>
 
 
 	
