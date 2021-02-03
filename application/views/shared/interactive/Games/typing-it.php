4857ebb10c6b0dde0b8c897e35f78daf15a7f5e0
Aphrodite Gajo <aprogajo@gmail.com>
2021-02-03 16:40:44 +0800

    Update typing it

--- application/views/shared/interactive/Games/typing-it.php
+++ application/views/shared/interactive/Games/typing-it.php
@@ -8,17 +8,25 @@
 		<h1> Type IT</h1>
 		<p>Type as many words as you can until time runs out!</p>
 
-		<button class="start" > START </button>
+		<button class="start btn-outline-secondary col-sm-3 col-md-3">
+			<label class="col-sm-12 col-md-12 col-form-label mt-2">
+				 START
+			</label>
+		</button>
+	
+	<div class="custom-select">
+			<!-- <select name="difficulty outline-secondary"> -->
 		<select name="difficulty">
 			<option value="">  Select Difficulty </option>
 			<option value="1">Easy ( 1p each )</option>
 			<option value="2">Moderate ( 2p each ) </option>
 			<option value="3">Hard ( 3p each )</option>
 		</select>
+	</div>
 		<div class="outerWrap">
 			<div class="scoreWrap">
 				<div class="scoreboard">
-					<img class="full-width" src="<?php echo base_url();  ?>/assets/images/games/score.jpg" alt="">
+					<img class="full-width" src="<?php echo base_url();  ?>/assets/images/games/score.png" alt="">
 				<div class="score-div">
 					<p class="mb-0">Score</p>
 					<span class="score">0</span>	
@@ -29,10 +37,10 @@
 
 			<div class="timeWrap">
 				<div class="scoreboard">
-					<img class="full-width" src="<?php echo base_url();  ?>/assets/images/games/score.jpg" alt="">
+					<img class="full-width" src="<?php echo base_url();  ?>/assets/images/games/score.png" alt="">
 				<div class="time-div">
 					<p class="mb-0">Time</p>
-					<span class="time">60</span>
+					<span class="score">60</span>
 				</div>
 			</div>
 		</div>
